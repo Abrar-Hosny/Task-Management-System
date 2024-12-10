557bcffa668b888e62e4da10a9a086e1be260f57
Abrar-Hosny <abrarhosny19@gmail.com>
2024-12-10 19:14:41 +0200

    Add Task form model and update extension of files into tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,7 @@ export default {
   		colors: {}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),
+    require('@tailwindcss/line-clamp')
+  ],
 }
