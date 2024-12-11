7e042f99cf9b75a41fb7f0cccbdf3a4f74a1088d
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-12-11 03:39:11 +0200

    Add Header component with user authentication dropdown menu

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../components/ui/dropdown-menu";
+import { Button } from "../components/ui/button";
+import { LogOut, User } from "lucide-react";
+import { useAuth } from "../context/AuthContext";
+
+export const Header: React.FC = () => {
+  const { user, logout } = useAuth();
+  console.log(user);
+  return (
+    <header className="flex items-center justify-end w-full p-4 border-b">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="rounded-full">
+            <User></User>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <div className="p-2 text-xs">
+            {user?.getSignInUserSession()?.getIdToken()?.payload?.email ||
+              "Guest"}
+          </div>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={logout}>
+            <LogOut className="w-4 h-4 mr-2" />
+            <span>Logout</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  );
+};
