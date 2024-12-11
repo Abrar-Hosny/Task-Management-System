aad3aaf4598bb96f7ef3d74f25f4bf2df0999502
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-12-12 01:10:26 +0200

    Refactor UserPool.ts to improve code readability by aligning property values

--- src/UserPool.ts
+++ src/UserPool.ts
@@ -4,8 +4,8 @@ import {
 } from "amazon-cognito-identity-js";
 
 const poolData: ICognitoUserPoolData = {
-  UserPoolId: "us-east-1_hUHDTfuHK", // Replace with your actual User Pool ID
-  ClientId: "sadaqu6rhgu6s76m8adt7vg6c", // Replace with your actual Client ID
+  UserPoolId: "us-east-1_hUHDTfuHK", 
+  ClientId: "sadaqu6rhgu6s76m8adt7vg6c", 
 };
 
 export default new CognitoUserPool(poolData);
